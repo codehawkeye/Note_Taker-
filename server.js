3866b1c87c64efe59af328c42c5a259e0ec13f33
Mike Carter <elite.mikecarter@gmail.com>
2021-01-26 23:49:50 -0500

    updated apiRoutes

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ const htmlRoutes = require("./routes/htmlRoutes");
 
 // app initialization and create port
 const app = express();
+// dynamic port for heroku deployment
+
 const PORT = process.env.PORT || 8080;
 
 
-// body parsing, static, and middleware
+// body parsing, static, and middleware for post route
 app.use(express.json());
 // URL encoding converts characters into a format that can be transmitted over the Internet
 app.use(express.urlencoded({ extended: true }));
 // serve static files such as images, CSS files, and JavaScript files.
 app.use(express.static("public"));
+// require our abstracted route folders
 app.use("/api", apiRoutes);
 app.use("/api", htmlRoutes);
 
