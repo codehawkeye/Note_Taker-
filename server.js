ce6669eefffdaff67cf69bf3e6df67f0b680d6d0
Mike Carter <elite.mikecarter@gmail.com>
2021-01-27 18:34:47 -0500

    changed files and path to correct folder

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("../routes/apiRoutes.js");
+const htmlRoutes = require("../routes/htmlRoutes.js");
+const fs = require("fs");
+const { send } = require("process");
+const newDb = [];
 
 // app initialization and create port
 const app = express();
@@ -16,8 +19,24 @@ app.use(express.urlencoded({ extended: true }));
 // serve static files such as images, CSS files, and JavaScript files.
 app.use(express.static("public"));
 // require our abstracted route folders
-app.use("/api", apiRoutes);
-app.use("/api", htmlRoutes);
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/assets/notes.html"));
+});
+    
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/assets/index.html"));
+
+});
+
+app.post("/api/notes", (req, require) => {
+    fs.readFile(path.join(__dirname, "/db/db/json"), "utf8, (err, data") = {
+        if(err) {
+            return console.log("Somthing went wrong!" + err)
+        }        
+        notes = JSON.parse(notes);
+        
+    }
+})
 
 
 
