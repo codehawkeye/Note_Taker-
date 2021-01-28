3f2525954dbac708d852440abb2a5432fabdea6e
Mike Carter <elite.mikecarter@gmail.com>
2021-01-28 15:53:16 -0500

    debugged apiRoute notes array populates now

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
-
+const fs = require("fs");
+var path = require("path")
 
 
 
@@ -7,20 +8,30 @@
 module.exports = function (app) {
 
     app.get("/api/notes", function (req, res) {
-        res.json("");
+       fs.readFile(path.join(__dirname + "/../db/db.json"), "utf8", (err, data) => {
+            if (err) throw err;
+            res.json(JSON.parse(data));
+    
+        })
+   
+    
     });
 
     app.post("/api/notes", (req, res) => {
         let newNote = req.body;
-        let noteList = json.parse(fs.readFileSync("./db/db.json"));
-        let noteLength = (notelist.length).toString();
-        
-        newNote.id = notelength;
-
+        console.log("this is the body" + req.body);
+        newNote.id = Math.floor(Math.random()* 1000);
+        let noteList = [];
         noteList.push(newNote);
 
-        fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
-        res.json(noteList);
+        fs.readFile(path.join(__dirname + "/../db/db.json"), "utf8", (err, data) => {
+            if (err) throw err;
+            let updated = noteList.concat(JSON.parse(data));
+            fs.writeFile(path.join(__dirname + "/../db/db.json"), JSON.stringify(updated), (err) => {
+                if (err) throw err;
+            })
+            res.json(updated);
+        })
     })
 
     app.delete("/api/notes/:id", (req, res) => {
--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 // dynamic port for heroku deployment
 
 const PORT = process.env.PORT || 8080;
+// body parsing, static, and middleware for post route
+app.use(express.json());
+// URL encoding converts characters into a format that can be transmitted over the Internet
+app.use(express.urlencoded({ extended: true }));
 
 // serve static files such as images, CSS files, and JavaScript files.
 app.use(express.static("public"));
@@ -17,13 +21,10 @@ app.use(express.static("public"));
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-// body parsing, static, and middleware for post route
-app.use(express.json());
-// URL encoding converts characters into a format that can be transmitted over the Internet
-app.use(express.urlencoded({ extended: true }));
+
 
 // require our abstracted route folders
 
-app.listen(PORT, () => console.log('listening on PORT: ${PORT}'));
+app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`));
 // app.listen(PORT, function() {
 //     console.log("App listening on PORT: " + PORT);
